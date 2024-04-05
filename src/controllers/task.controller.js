fe15e322cb39f7529e8c1c63716abfc0d61a487c
hirwajeaneric <hirwajeaneric25@gmail.com>
2024-04-05 13:13:02 +0200

    Feature: Add task functionality + API test

--- src/controllers/task.controller.js
+++ src/controllers/task.controller.js
@@ -1,9 +1,17 @@
+import TaskModel from '../models/task.model.js';
+
 export const test = (req, res, next) => {
     res.send('Hello World!');
 }
 
-export const addTask = (req, res, next) => {
+export const addTask = async(req, res, next) => {
+    try {
+        const newTask = await TaskModel.create(req.body);
+        return res.status(201).json(newTask);
 
+    } catch (error) {
+        return res.status(500).json({ message: error.message.split(":")[2].trim() });
+    }
 };
 
 export const getTasks = (req, res, next) => {
