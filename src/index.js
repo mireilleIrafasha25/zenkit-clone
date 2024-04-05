c0b841db2aaf48c59bc089e8c44b47fafd839b95
hirwajeaneric <hirwajeaneric25@gmail.com>
2024-04-05 12:42:58 +0200

    Features: Project setup
    The following are the tasks that are done so far:
    1. Creating the project
    2. Installing all needed packages
    3. Setting environment variables
    4. Testing the server
    5. Creating the task Model

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import configurations from "./configs/index.js";
+
+const corsOptions = {
+    allowedHeaders: ["Authorization","Content-Type"],
+    methods: ["GET", "POST", "UPDATE" ],
+    origin: ["http://192.168.1.150:8080", "//https://contact-app-client-xbck.onrender.com/"],
+}
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect("mongodb://localhost:27017/zenkit")
+.then(() => {
+    console.log("Connected to MongoDB");
+})
+.catch(err => {
+    console.log(err);
+})
+
+app.listen(configurations.PORT, () => {
+    console.log(`Server is running on port ${configurations.PORT}`);
+})
