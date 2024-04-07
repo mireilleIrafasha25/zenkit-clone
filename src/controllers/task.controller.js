3eb8f7cd012829bf0482161982bbd7ddf0e5b6f8
mireilleIrafasha25 <156303827+mireilleIrafasha25@users.noreply.github.com>
2024-04-07 22:29:53 +0200

    First commit

--- src/controllers/task.controller.js
+++ src/controllers/task.controller.js
@@ -9,19 +9,45 @@ export const addTask = async(req, res, next) => {
         const newTask = await TaskModel.create(req.body);
         return res.status(201).json(newTask);
 
-    } catch (error) {
+    } 
+    catch (error) {
         return res.status(500).json({ message: error.message.split(":")[2].trim() });
     }
 };
 
-export const getTasks = (req, res, next) => {
-
+export const getTasks = async(req, res, next) => {
+try{
+    const tasks=await TaskModel.find()
+    res.status(200).json({task:tasks})
+}
+catch(error)
+{
+console.log(error.message)
+res.status(500).json({message:"Internal server error"})
+}
 }
 
-export const updateTask = (req, res, next) => {
+export const updateTask = async(req, res, next) => {
+try{
+    const updatedtask=await TaskModel.findByIdAndUpdate(req.params.id,req.body,{set:true})
+    res.status(200).json({task:updatedtask})
+}
+catch(error)
+{
 
+    console.log(error.message)
+    res.status(500).json({message:"Internal server error"})
+}
 }
 
-export const deleteTask = (req, res, next) => {
-
+export const deleteTask = async(req, res, next) => {
+const deletedTask=await TaskModel.findByIdAndDelete(req.params.id)
+try{
+    res.status(200).json({task:deletedTask})
+}
+catch(error)
+{
+    console.log(error.message)
+    res.status(500).json("Internal server error")
+}
 }
